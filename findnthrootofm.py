00bc3801bdff3a3965e57c9d5374f660e7ad0d3b
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-06-28 18:24:56 +0530

    Easy

---
+++ findnthrootofm.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+	def nthRoot(self, n: int, m: int) -> int:
+		low = 1
+		high = m
+		while (low <= high):
+		    mid = (low + high) // 2
+		    if mid ** n == m:
+		        return mid
+		    else:
+		        if mid ** n > m:
+		            high = mid - 1
+		        else:
+		            low = mid + 1
+		            
+        return -1 
