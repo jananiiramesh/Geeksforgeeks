7277ee31e3334040416831ebafadd4ee07a0ba1a
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-07-11 11:49:40 +0530

    Hard

--- medianInARowWiseSortedArr.py
+++ medianInARowWiseSortedArr.py
@@ -1,8 +1,29 @@
 #User function Template for python3
 
+from bisect import bisect_right
 class Solution:
     def median(self, mat):
-    	mat_1d = [ele for sublist in mat for ele in sublist]
-    	mat_1d.sort()
-    	low, high = 0, len(mat_1d) - 1
-    	return mat_1d[(low + high) // 2]
+    	
+    	low = min(row[0] for row in mat)
+    	high = max(row[-1] for row in mat)
+    	
+    	n = len(mat)
+    	m = len(mat[0])
+    	
+    	pos = (n*m+1)//2
+    	
+    	while (low < high):
+    	    mid = (low + high) // 2
+    	    
+    	    count = 0
+    	    for row in mat:
+    	        count += bisect_right(row, mid)
+    	        
+    	    if count < pos:
+    	        low = mid + 1
+    	    
+    	    else:
+    	        high = mid
+    	        
+    	        
+    	return low
