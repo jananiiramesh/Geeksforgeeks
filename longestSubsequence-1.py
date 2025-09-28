15d8c1331c47675f4667c03ad0dd5399e398922f
jananiiramesh <164179516+jananiiramesh@users.noreply.github.com>
2025-09-28 19:05:26 +0530

    Medium

---
+++ longestSubsequence-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestSubseq(self, arr):
+        # recursive solution
+        n = len(arr)
+        # dp = [[-1] * (n) for _ in range(n)]
+        
+        # def recursive(ind, prev_ind):
+        #     if ind == n:
+        #         return 0
+                
+        #     if dp[ind][prev_ind + 1] != -1:
+        #         return dp[ind][prev_ind+1]
+                
+        #     notTake = recursive(ind+1, prev_ind)
+        #     take = 0
+        #     if prev_ind == -1 or abs(arr[ind] - arr[prev_ind]) == 1:
+        #         take = 1 + recursive(ind+1, ind)
+                
+        #     dp[ind][prev_ind+1] = max(take, notTake)
+        #     return dp[ind][prev_ind+1]
+            
+        # return recursive(0, -1)
+        
+        dp = [[0] * (n+1) for _ in range(n+1)]
+        
+        for ind in range(n-1, -1, -1):
+            for prev_ind in range(ind-1, -2, -1):
+                notTake = dp[ind+1][prev_ind+1]
+                take = 0
+                if prev_ind == -1 or abs(arr[ind] - arr[prev_ind]) == 1:
+                    take = 1 + dp[ind+1][ind+1]
+                    
+                dp[ind][prev_ind+1] = max(notTake, take)
+                
+        return dp[0][0]
+        
